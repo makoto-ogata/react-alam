cce79f1f284cff3ddc8c033cb3fe69eade21e9ef
Makoto Ogata <ogatamakoto1976@gmail.com>
2021-07-25 23:54:59 +0900

    create nowTime constants

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from 'react';
 
 function App() {
   const [showTime, setShowTime] = useState(new Date());
+  const [time, setTime] = useState('');
+  const handleTime =(e)=> setTime(e.target.value);
+
   useEffect(() => {
     const timer = setInterval(()=> {
       setShowTime(new Date());
@@ -9,12 +12,11 @@ function App() {
     return ()=> clearInterval(timer);
   }, [setShowTime]);
 
-  const [time, setTime] = useState('');
-  const handleTime =(e)=> setTime(e.target.value);
+  const nowTime = showTime.toLocaleTimeString([],{hour: 'numeric', minute:'numeric'});
   return (
     <div className="App">
       <p>現在の時刻</p>
-      <p>{showTime.toLocaleTimeString([],{hour: 'numeric', minute:'numeric'})}</p>
+      <p>{nowTime}</p>
       <input type="time" value={time} onChange={handleTime} />
     </div>
   );
