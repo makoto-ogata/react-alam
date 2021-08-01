ebd14c5bd8ac6a7ebbc8900c3e908773d0816c21
Makoto Ogata <ogatamakoto1976@gmail.com>
2021-08-01 23:51:33 +0900

    added styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import sound from './sound/piano-without-melody-short-loop-60-bpm.mp3';
 import Alarm from './components/Alarm';
+import styled from 'styled-components';
+import './App.css';
+
 const alarmSound = new Audio(sound);
 alarmSound.loop = true;
 
-function App() {
+let dateMessage = {
+  images: '',
+  text: ''
+}
+
+const App =()=> {
   const [showTime, setShowTime] = useState(new Date());
   const [time, setTime] = useState('');
   const handleTime =(e)=> setTime(e.target.value);
@@ -27,17 +35,44 @@ function App() {
   }, []);
 
   const nowTime = showTime.toLocaleTimeString([],{hour: 'numeric', minute:'numeric'});
+  const nowTimeHour = showTime.toLocaleTimeString([],{hour: 'numeric'});
+  const nowTimeHourNumber = parseFloat(nowTimeHour);
 
   useEffect(()=>{
    if(nowTime === alarmTime){
      alarmSound.play();
    }
   }, [nowTime]);
+
+  useEffect(()=>{
+    if(nowTimeHourNumber > 18 || nowTimeHourNumber < 6) {
+      dateMessage = {
+        images: 'https://source.unsplash.com/VZxNq9GytpQ',
+        text: 'Good Evening'
+      }
+    }else if(nowTimeHourNumber > 6 || nowTimeHourNumber < 12){
+      dateMessage = {
+        images: 'https://source.unsplash.com/-G3rw6Y02D0',
+        text: 'Good Morning'
+      }
+    } else {
+      dateMessage = {
+        images: 'https://source.unsplash.com/8GVuQUmZW8Y',
+        text: 'Good Afternoon'
+      }
+    }
+  }, [nowTimeHourNumber]);
+
   return (
-    <div className="App">
-      <Alarm nowTime={nowTime} time={time} handleTime={handleTime} handleAdd={handleAdd} stopSound={stopSound} alarmTime={alarmTime} />
-    </div>
+    <Main style={{backgroundImage: `url(${dateMessage.images})`}}>
+      <Alarm dateMessage={dateMessage} nowTime={nowTime} time={time} handleTime={handleTime} handleAdd={handleAdd} stopSound={stopSound} alarmTime={alarmTime} />
+    </Main>
   );
 }
 
+const Main = styled.main`
+  height: 100vh;
+  width: 100%;
+  background-size: cover;
+`
 export default App;
--- src/components/Alarm.js
+++ src/components/Alarm.js
@@ -1,18 +1,66 @@
+import styled from 'styled-components';
 
 const Alarm =(props)=> {
-  const {nowTime,time, handleTime, handleAdd, stopSound, alarmTime} = props;
+  const {nowTime,time, handleTime, handleAdd, stopSound, alarmTime, dateMessage} = props;
   return(
-   <>
-     <p>現在の時刻</p>
-     <p>{nowTime}</p>
-     <input type="time" value={time} onChange={handleTime} />
-     <button type="submit" onClick={handleAdd}>Set</button>
-     <button type="submit" onClick={stopSound}>Stop</button>
-     <div>
-       {alarmTime}
-     </div>
-   </>
+      <Content>
+        <Message>{dateMessage.text}</Message>
+        <DefaultText>The current time is</DefaultText>
+        <TimeText>{nowTime}</TimeText>
+        <DefaultText>Please, set the alarm</DefaultText>
+        <SetTime type="time" value={time} onChange={handleTime} />
+        <ButtonArea>
+          <SetButton type="submit" onClick={handleAdd}>SetAlarm</SetButton>
+          <SetButton type="submit" onClick={stopSound}>StopAlarm</SetButton>
+        </ButtonArea>
+        <TimeText>{alarmTime}</TimeText>
+      </Content>
   )
 }
 
+const Content = styled.div`
+  display: block;
+  max-width: 800px;
+  margin: 0 auto;
+  color: #fff;
+  padding-top: 30px;
+  text-align: center;
+`
+
+const Message = styled.h2`
+  font-size: 45px;
+  text-shadow: 1px 0 2px #333;
+`
+
+const DefaultText = styled.p`
+  margin-top: 20px;
+  font-size: 20px;
+  text-shadow: 1px 0 2px #333;
+`
+
+const TimeText = styled.p`
+  font-size: 30px;
+  margin-top: 30px;
+  text-shadow: 1px 0 2px #333;
+`
+
+const SetTime = styled.input`
+  border: none;
+  padding: 5px;
+  margin-top: 20px;
+`
+
+const ButtonArea = styled.div`
+  display: flex;
+  justify-content: center;
+  gap: 20px;
+  margin-top: 30px;
+`
+
+const SetButton = styled.button`
+  border: none;
+  font-size: 18px;
+  border-radius: 5px;
+  padding: 5px 10px;
+`
 export default Alarm;
